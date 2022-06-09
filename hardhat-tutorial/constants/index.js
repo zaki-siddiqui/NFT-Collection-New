50f99dd73ba4b6b023e1334bd30b453aeacb8f5c
Zaki Siddiqui <xaki85@gmail.com>
2022-06-09 18:26:49 +0500

    NFT Collection New

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xe63baf2e6901857bbBFf755334E669a005e5564E";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
